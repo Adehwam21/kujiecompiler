dca81ef61df7e17cb30d863231acdb28943d3339
Adehwam21 <drewaaron96@gmail.com>
2025-08-27 12:17:08 +0000

    add entry file

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+from pathlib import Path
+from utils.helpers import read_source_file
 from utils.lexer import Lexer
 from utils.parser import Parser
+from utils.codegen import Codegen
 
-SOURCE = "make x = 3;wrep x;if x > 0 do wrep \"greater\"; else do wrep \"lesser\";"
+src_path = Path("examples") / "new.kj"
+src = read_source_file(src_path)
 
+# Lexing, parsing, codegen
+lex = Lexer(src)
+tokens = lex.tokenize_as_object()
+parser = Parser(tokens)
+prog = parser.parse_program()
+cg = Codegen()
 
-lex = Lexer(SOURCE)
-parser = Parser(lex.tokenize_as_object())
-
-parser.print_ast()
+cg.generate(prog)
+# cg.dump_ir()              # optionally print LLVM IR
+cg.write_ir("out.ll")       # optionally save LLVM IR
+cg.jit_run()                # execute the program
